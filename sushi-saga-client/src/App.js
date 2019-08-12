72866f1e88a5ad98a6ad59af6ade816fe772a0eb
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-12 16:40:20 -0400

    Added the Sushi

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,14 +7,41 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+	state = {
+		allSushi: [],
+		eaten: [],
+		index: 0,
+		loaded: false
+	}
+
+	showSushi = () => {
+		if (this.state.allSushi.length > 0) {
+			let count = this.state.index;
+			let sushi = [];
+			while (count < (this.state.index + 4)) {
+				sushi.push(this.state.allSushi[count++])
+			}
+			return sushi;
+		}
+		return this.state.allSushi;
+	}
+
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
+      	{this.state.loaded ? <SushiContainer  sushi={this.showSushi()}/> : <h1>Loading...</h1>}
+        
         <Table />
       </div>
     );
   }
+
+  componentDidMount = () => {
+		fetch(API)
+		.then(res => res.json())
+		.then(allSushi => this.setState({...this.state, allSushi, loaded: true}))
+
+	}
 }
 
 export default App;
